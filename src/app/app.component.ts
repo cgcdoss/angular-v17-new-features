f66330c1d4b2f8941006e96731643bc8737b9a7b
cgcdoss <gabi04061997@gmail.com>
2024-04-19 17:49:34 -0300

    feat: adicionando Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,12 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-v17-new-features';
+
 }
