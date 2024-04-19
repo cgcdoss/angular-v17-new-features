9fa7bb9d13bef76ef95b40f2770d5c5abbf1f93e
cgcdoss <gabi04061997@gmail.com>
2024-04-19 19:01:27 -0300

    feat: novo component Todo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,9 @@ export const routes: Routes = [
     path: 'cart',
     loadComponent: () => import('./pages/cart/container/cart.component').then(c => c.CartComponent),
   },
+  {
+    path: 'todo',
+    loadComponent: () => import('./pages/todo/container/todo.component').then(c => c.TodoComponent),
+  },
   { path: '', redirectTo: 'cart', pathMatch: 'full' },
 ];
