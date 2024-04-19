f5c8694478aeb6acec30a8fd7a9b2e8c0558021d
cgcdoss <gabi04061997@gmail.com>
2024-04-19 19:24:41 -0300

    feat: uso da nova funcionalidade @defer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,5 +9,9 @@ export const routes: Routes = [
     path: 'todo',
     loadComponent: () => import('./pages/todo/container/todo.component').then(c => c.TodoComponent),
   },
+  {
+    path: 'defer',
+    loadComponent: () => import('./pages/defer/container/defer.component').then(c => c.DeferComponent),
+  },
   { path: '', redirectTo: 'cart', pathMatch: 'full' },
 ];
