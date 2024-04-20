9546ffb70af2ea7ffbfd1fdce8298adfcaedc966
cgcdoss <gabi04061997@gmail.com>
2024-04-19 22:50:13 -0300

    chore: usando hashLocation para evitar problema de 404 no Github Pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideRouter, withHashLocation, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withViewTransitions()),
+    provideRouter(routes, withViewTransitions(), withHashLocation()),
     provideAnimationsAsync(),
   ]
 };
